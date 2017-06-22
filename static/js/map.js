819c16ffa508cd375f482f6a703efbd2bbbf8fc1
AnthonySchiesz <aschiesz91@gmail.com>
2017-06-22 15:27:05 -0400

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1,192 +1,30 @@
-var customIcons = {
-// icons to be associated with future century content	
-//	1200: {
-//		icon: 'http://maps.google.com/mapfiles/ms/icons/pink.png'
-//	},
-//	1300: {
-//		icon: 'http://maps.google.com/mapfiles/ms/icons/lightblue.png'
-//	},
-//	1400: {
-//		icon: 'http://maps.google.com/mapfiles/ms/icons/orange.png'
-//	},
-//	1500: {
-//		icon: 'http://maps.google.com/mapfiles/ms/icons/red.png'
-//	},
-	1600: {
-		icon: 'http://maps.google.com/mapfiles/ms/icons/purple.png'
-	},
-	1700: {
-		icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png'
-	},
-	1800: {
-		icon: 'http://maps.google.com/mapfiles/ms/icons/green.png'
-	},
-	1900: {
-		icon: 'http://maps.google.com/mapfiles/ms/icons/yellow.png'
-	}
-};
-
-var markerGroups = {
-	"1600": [],
-	"1700": [],
-	"1800": [],
-	"1900": []
-};
-
 //Load map with custom style settings
-function initialize() {            
+function initialize() {
 	var map = new google.maps.Map(document.getElementById("map"), {
 		center: new google.maps.LatLng(40.7126168, -74.0058063),
 		zoom: 13,
+		zoomControl: true,
 		disableDefaultUI: true,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: style //custom styling located in the model.js
 	});
 
-	map.set('styles', [{
-		zoomControl: false
-	}, {
-				featureType: "administrative.land_parcel",
-		elementType: "labels.text",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "poi.attraction",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "poi.business",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "poi.government",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "poi.medical",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "poi.park",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "poi.place_of_worship",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "poi.school",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "poi.sports_complex",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "road.arterial",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "road.highway",
-		stylers: [{
-			visibility: "simplified"
-		}]
-	}, {
-		featureType: "road.highway",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "road.highway.controlled_access",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "road.local",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "transit",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "transit.line",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "transit.station",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "transit.station.airport",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "transit.station.bus",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "transit.station.rail",
-		elementType: "labels.icon",
-		stylers: [{
-			visibility: "off"
-		}]
-	}, {
-		featureType: "water",
-		elementType: "geometry.fill",
-		stylers: [{
-			color: "#97cbff"
-		}]
-	}]);
 	var infoWindow = new google.maps.InfoWindow();
 
-	downloadUrl("static/xml/markers.xml", function (data) {
-	// var xml = xmlParse("markers.xml");
-	var xml = data.responseXML;
-	var markers = xml.documentElement.getElementsByTagName("marker");
 	for (var i = 0; i < markers.length; i++) {
-		var name = markers[i].getAttribute("name");
-		var detail = markers[i].getAttribute("detail");
-		var image = markers[i].getAttribute("image");
-		var type = markers[i].getAttribute("type");
-		var link = markers[i].getAttribute("link");
+		var name = markers[i].name;
+		var detail = markers[i].detail;
+		var image = markers[i].image;
+		var type = markers[i].type;
+		var link = markers[i].link;
+		var marker = new google.maps.Marker({
+			map: map,
+			position: new google.maps.LatLng(lat, lng),
+			title: name,
+			animation: google.maps.Animation.DROP,
+		});
 
-	var point = new google.maps.LatLng(
-		parseFloat(markers[i].getAttribute("lat")),
-		parseFloat(markers[i].getAttribute("lng")));
-
+	// infowindow formated under 'var html' so it would appear in sidebar instead of pop-up infowindow over map.
 	var html = "<div id='siteinfo'>" +   
 		"<h3>" + name + "</h3>" + 
 		"</b>" +
@@ -200,43 +38,78 @@ function initialize() {
 	var siteinfo = 
 		bindInfoWindow(marker, map, infoWindow, html);
 	}
-	});
+
+
 	google.maps.event.addDomListener(window, 'load', initialize);
 };
 
-//Defining the marker
-function createMarker(point, name, type, map) {
-	var icon = customIcons[type] || {};
-	var marker = new google.maps.Marker({
-		map: map,
-		position: point,
-		icon: icon.icon,
-		title: name,
-		type: type
-	});
+//--VIEW--//
 
-	if (!markerGroups[type]) markerGroups[type] = [];
-	markerGroups[type].push(marker);
+var markerView = {
+	init: function() {
+		// store markers to DOM elements for easy access later
+		this.markerElem = document.getElementById('type');
+		this.markerNameElem = document.getElementById('name');
+		this.markerLatElem = document.getElementById('lat');
+		this.markerLngElem = document.getElementById('lng');
+		this.markerDetailElem = document.getElementById('detail');
+		this.markerLinkElem = document.getElementById('link');
+	},
 
-	return marker;
-}
+	render: function() {
+		// update the DOM elements with the values from the current marker
+		var currentMarker = octopus.getCurrentMarker();
+		this.markerElem.textContent = currentMarker.type;
+		this.markerLatElem.textContent = currentMarker.lat;
+		this.markerLngElem.textContent = currentMarker.lng;
+	}
+};
 
-//Filter the markers through navbar dropdown div. Purpose is to click one century 
-// to show, then hide others not related to active selection.
+var markerListView = {
 
-function filterMarkers(type) {
-	for (i = 0; i < markerGroups[type].length; i++) {
-		marker = markerGroups[type][i];
-		// If is same century or century not picked
-		if (marker.type == type || type.length === 0) {
-			marker.setVisible(true);
-		}
-		// Centuries (type) don't match 
-		else {
-			marker.setVisible(false);
+	init: function() {
+		// store the DON element for easy access later
+		self.markerListElem = ko.observableArray([
+			{type: "1600"},
+			{type: "1700"},
+			{type: "1800"},
+			{type: "1900"}
+		])
+
+	},
+
+	render: function() {
+		var marker, elem, i;
+		// get the markers we'll be rendering from the octopus
+		var markers = octopus.getMarkers();
+
+		// empy the marker list
+		this.markerListElem.innterHTML = '';
+
+		// loop over the markers
+		for (i = 0; i < markers.length; i++) {
+			// this is the marker we are currently looping over
+			marker = markers[i];
+
+			// make a new marker list item and set its text
+			elem = document.createElement('li');
+			elem.textContent = marker.name;
+
+			// on click, setCurrentMarker and render the markerView
+			// (this uses our closure-in-a-loop trick to connect the value
+			// of the marker variable to the click event function)
+			elem.addEventListener('click', (function(markerCopy) {
+				return function() {
+					octopus.setCurrentMarker(markerCopy);
+					markerView.render();
+				};
+			})(marker));
+
+			// finally, add the element to the list
+			this.markerListElem.appendChild(elem);
 		}
 	}
-}
+};
 
 // Attach and display infoWindow when marker clicked. Info window displayed in sidebar.
 function bindInfoWindow(marker, map, infoWindow, html, link) {
@@ -245,34 +118,4 @@ function bindInfoWindow(marker, map, infoWindow, html, link) {
 		document.getElementById('siteinfo').innerHTML = html + "</br><span><a href='javascript:void(null)' onclick='closeInfo()'>Close</a></span></br>";
 		document.getElementById('siteinfo').style.width = "255px";
 	});
-}
-
-
-function downloadUrl(url, callback) {
-	var request = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest();
-
-	request.onreadystatechange = function() {
-		if (request.readyState == 4) {
-			request.onreadystatechange = doNothing;
-			callback(request, request.status);
-		}
-	};
-
-	request.open('GET', url, true);
-	request.send(null);
-}
-function doNothing() {}
-
-function xmlParse(str) {
-	if (typeof ActiveXObject != 'undefined' && typeof GetObject != 'undefined') {
-		var doc = new ActiveXObject('Microsoft.XMLDOM');
-		doc.loadXML(str);
-		return doc;
-	}
-
-	if (typeof DOMParser != 'undefined') {
-		return (new DOMParser()).parseFromString(str, 'text/xml');
-	}
-
-	return createElement('div', null);
 }
