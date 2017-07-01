af45b9be59415deb339f30fe21532b184469c723
AnthonySchiesz <aschiesz91@gmail.com>
2017-07-01 18:44:52 -0400

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1,20 +1,26 @@
+//** MVVM **//
+
+//// MODEL ////
 var historicalLocation = [
 		{
-			type : "1600",
+			type : "17th Century",
 			name : "Fort Amsterdam",
 			location : {lat: 40.704100, lng: -74.013753},
 			detail : "First military installation in New York.",
 			link : "https://en.wikipedia.org/wiki/Fort_Amsterdam"
 		},
 		{
-			type : "1700",
-			name : "St. Paul\'s Chapel of Trinity Church",
+			type : "18th Century",
+			name : "St. Pauls Chapel of Trinity Church",
 			location : {lat: 40.711313, lng: -74.009190},
 			detail : "The chruch President George Washington celebrated mass after being inagurated.",
 			link : "https://en.wikipedia.org/wiki/St._Paul%27s_Chapel"
 		}
 
 ];
+
+
+var customIcon = 'http://maps.google.com/mapfiles/ms/icons/red.png';
 
 //// MAP ////
 //Load map with custom style settings
@@ -25,21 +31,31 @@ function init() {
 		zoomControl: true,
 		disableDefaultUI: true,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		styles: style //custom styling located in the model.js
+		styles: style //custom styling located in the style.js
 	});
 
 	for (i = 0; i < historicalLocation.length; i++) {
 		var type = historicalLocation[i].type;
 		var name = historicalLocation[i].name;
 		var location = historicalLocation[i].location;
-		var position = historicalLocation[i].location;
+		var detail = historicalLocation[i].detail;
+		var link = historicalLocation[i].link;
 
 		var marker = new google.maps.Marker({
-			location: location,
 			map: map,
+			location: location,
+			type: type,
 			name: name,
+			icon: customIcon,
 			animation: google.maps.Animation.DROP
 		});
+		// to bind to infowindow. Infowindow to display as sidebar.
+		var html = "<div id='siteinfo'>" +   
+			"<h3>" + name + "</h3>" + 
+			"</b>" +
+			"<p>" + detail + "</p>" +
+			"<a href='" + link + "'>More...</a>" +
+			"</b>" + "</div>";
 //		marker.setVisible(false)
 
 	historicalLocation[i].marker = marker;
@@ -48,26 +64,18 @@ function init() {
 
 	var siteinfo = 
 		bindInfoWindow(marker, map, infoWindow);
+
+
+	// Attach and display infoWindow when marker clicked. Info window displayed in sidebar.
+	function bindInfoWindow(marker, map, infoWindow, html, link) {
+		// display site details in siteInfo bar
+		google.maps.event.addListener(marker, 'click', function() {
+			document.getElementById('siteinfo').innerHTML = html + "</br><span><a href='javascript:void(null)' onclick='closeInfo()'>Close</a></span></br>";
+			document.getElementById('siteinfo').style.width = "255px";
+		});
+	}
 	
 	ko.applyBindings(viewModel);
-};
-
-// Attach and display infoWindow when marker clicked. Info window displayed in sidebar.
-function bindInfoWindow(marker, map, infoWindow, html, link) {
-	// display site details in siteInfo bar
-	google.maps.event.addListener(marker, 'click', function() {
-		document.getElementById('siteinfo').innerHTML = html + "</br><span><a href='javascript:void(null)' onclick='closeInfo()'>Close</a></span></br>";
-		document.getElementById('siteinfo').style.width = "255px";
-	});
-}
-
-//// VIEW ////
-var view = function(data) {
-	this.type = ko.observable(century);
-	this.name = ko.observable(name);
-	this.location = ko.observable(location);
-	this.marker = data.marker;
-
 };
 
 //// VIEW MODEL ////
@@ -91,7 +99,7 @@ var viewModel  = function() {
 
 			} else {
 				// Centuries (type) don't match
-				selectedCentury.isVisible(true);
+				selectedCentury.setVisible(true);
 			}
 		}
 	})
