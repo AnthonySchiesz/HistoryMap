e0b8b5ab0f1fec218ed5d15a65a312d1fa3daa7a
AnthonySchiesz <aschiesz91@gmail.com>
2017-07-16 01:16:18 -0400

    Update sidenav.js

--- static/js/sidenav.js
+++ static/js/sidenav.js
@@ -1,10 +1,10 @@
 //Siteinfo to read details about site
-function openInfo() {
-	document.getElementById("siteinfo").style.width = "250px";
-	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
+//function openInfo() {
+//	document.getElementById("bar").style.width = "250px";
+//	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+//}
 
 function closeInfo() {
-	document.getElementById("siteinfo").style.width = "0";
+	document.getElementById("bar").style.width = "0";
 	document.body.style.backgroundColor = "white";
 }
