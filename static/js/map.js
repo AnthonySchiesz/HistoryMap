b8777069006ea04978b25c099005e97c3838714b
AnthonySchiesz <aschiesz91@gmail.com>
2017-07-16 01:15:18 -0400

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -6,7 +6,6 @@ var historicalLocation = [
 			type : "17th Century",
 			name : "Fort Amsterdam",
 			coord : {lat: 40.704100, lng: -74.013753},
-			pageid : "https://en.wikipedia.org/wiki/Fort_Amsterdam/",
 			page : "Fort_Amsterdam",
 			link : "https://en.wikipedia.org/wiki/Fort_Amsterdam"
 		},
@@ -14,7 +13,6 @@ var historicalLocation = [
 			type : "18th Century",
 			name : "St. Pauls Chapel of Trinity Church",
 			coord : {lat: 40.711313, lng: -74.009190},
-			pageid : "https://en.wikipedia.org/wiki/St._Paul%27s_Chapel/",
 			page : "St._Paul%27s_Chapel",
 			link : "https://en.wikipedia.org/wiki/St._Paul%27s_Chapel"
 		},
@@ -22,7 +20,6 @@ var historicalLocation = [
 			type : "19th Century",
 			name : "Statue of Liberty",
 			coord : {lat: 40.689250, lng: -74.044480},
-			pageid : "https://en.wikipedia.org/wiki/Statue_of_Liberty",
 			page : "Statue_of_Liberty",
 			link : "https://en.wikipedia.org/wiki/Statue_of_Liberty"
 		},
@@ -30,7 +27,6 @@ var historicalLocation = [
 			type : "19th Century",
 			name : "Brooklyn Bridge",
 			coord : {lat: 40.706084, lng: -73.996864},
-			pageid : "https://en.wikipedia.org/wiki/Brooklyn_Bridge",
 			page : "Brooklyn_Bridge",
 			link : "https://en.wikipedia.org/wiki/Brooklyn_Bridge"
 		},
@@ -38,7 +34,6 @@ var historicalLocation = [
 			type : "19th Century",
 			name : "Manhattan Bridge",
 			coord : {lat: 40.707497, lng: -73.990773},
-			pageid : "https://en.wikipedia.org/wiki/Manhattan_Bridge",
 			page : "Manhattan_Bridge",
 			link : "https://en.wikipedia.org/wiki/Manhattan_Bridge"
 		},
@@ -46,7 +41,7 @@ var historicalLocation = [
 			type : "20th Century",
 			name : "SS Normandie",
 			coord : {lat: 40.766782, lng: -73.998989},
-			pageid : "https://en.wikipedia.org/wiki/SS_Normandie",
+			event : "At 14:30 on 9 February 1942, the SS Normandie catches fire as sparks from a welding torch ignited a stack of life vests filled with flammable kapok that had been stored in the first-class lounge. The ship had a very efficient fire protection system, but it had been disconnected during its conversion. Fire spread to 3 upper decks within an hour and the Normandie began to list as firefighters on shore and in fire boats poured water on the blaze. The Normandie capsized on 10 February 1942. One man died in the tragedy — Frank 'Trent' Trentacosta, 36, of Brooklyn. 38 fire fighters and 153 civilians were treated for various injuries, burns, smoke inhalation, and exposure.",
 			page : "SS_Normandie",
 			link : "https://en.wikipedia.org/wiki/SS_Normandie"
 		}
@@ -70,7 +65,7 @@ function init() {
 		var type = historicalLocation[i].type;
 		var name = historicalLocation[i].name;
 		var location = historicalLocation[i].coord;
-		var pageid = historicalLocation[i].pageid;
+		var event = historicalLocation[i].event;
 		var page = historicalLocation[i].page;
 		var link = historicalLocation[i].link;
 
@@ -85,62 +80,56 @@ function init() {
 		});
 		// to bind to infowindow. Infowindow to display as sidebar.
 		var html = "<div id='siteinfo'>" +   
-			"<h4>" + name + "</h4>";
+			"<h5>" + event + "</h5>";
 
-		var path = "<div id='siteinfo'>" + "<a href='" + link + "'>More...</a>" + "</div>";
+		var path = "<div id='siteinfo'>" + "<span><a href='" + link + "'>Source</a><span>" + "</div>";
+
 		// marker.setVisible(false)
 
 		historicalLocation[i].marker = marker;
 	}
-	var infoWindow = new google.maps.InfoWindow();
-//	var siteinfo = 
-//		bindInfoWindow(marker, map, infoWindow, html);
-
-	//// WIKIPEDIA ////
-	// **WORK IN PROGRESS** //
-
-	$(document).ready(function () {
-		$('#list').click(function () {
-			$('#siteinfo').empty();
-			$.ajax({
-				url: 'http://www.mediawiki.org/w/api.php',
-				dataType: 'jsonp',
-				data: {
-					action: 'query',
-					format: 'json',
-					generator: 'random',
-					prop: 'extracts',
-					grnnamespace: 0,
-					explaintext: true
-				},
-				success: function (result) {
-					var pages = result.query.pages;
-					var page = pages[Object.keys(pages)[0]];
-					$('#siteinfo').append($('<h3>').text(page.title));
-					$('#siteinfo').append($('<p>').text(page.extract));
-				}
-			});
-		});
-	});
-
-
+	var infoWindow = bindInfoWindow(marker, map, infoWindow, html);
 
 	// Attach and display infoWindow when marker clicked. Info window displayed in sidebar.
-//	function bindInfoWindow(marker, map, infoWindow, html, link) {
+	function bindInfoWindow(marker, map, infoWindow, html, link) {
 		// display site details in siteInfo bar
-//		google.maps.event.addListener(marker, 'click', function() {
+		google.maps.event.addListener(marker, 'click', function() {
 			// Pan to marker on Location List item click
-//			map.setZoom(13);
-//			map.setCenter(marker.getPosition());
+			map.setZoom(13);
+			map.setCenter(marker.getPosition());
 			// Format infoWindow through sidebar.
-//			document.getElementById('siteinfo').innerHTML = html + "<p>" + page + "</p>" + path + 
-//			"</br><span><a href='javascript:void(null)' onclick='closeInfo()'>Close</a></span></br>";
-//			document.getElementById('siteinfo').style.width = "335px";
-//		});
-		//wikiInfo(marker, siteinfo)
-//	}
-	
-//	ko.applyBindings(new viewModel());
+			document.getElementById('siteinfo').innerHTML = "</br>" + "<h3></h3>" + event + "<p></p>" + "</br>" + path;
+			document.getElementById('bar').style.width = "335px";
+		
+			//// WIKIPEDIA API////
+			// **WORK IN PROGRESS** //
+			$(document).ready(function(historicalLocation) {
+				$('#siteinfo').empty();
+				$.ajax({
+					url: 'http://en.wikipedia.org/w/api.php',
+					dataType: 'jsonp',
+					data: {
+						action: 'query',
+						format: 'json',
+						titles: page,
+						prop: 'extracts',
+						exintro: 0,
+						grnnamespace: 0,
+						explaintext: true
+					},
+					success: function(result) {
+						var pages = result.query.pages;
+						var page = pages[Object.keys(pages)[0]];
+						$('#siteinfo').append($('<h3>').text(page.title));
+						$('#siteinfo').append($('<p>').text(page.extract));
+					}
+
+				});
+			});
+		});
+	}
+
+	ko.applyBindings(new viewModel());
 };
 
 
