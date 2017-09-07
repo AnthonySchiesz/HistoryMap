c9d7b584181d94cd2766a877a20020843e3f73d2
AnthonySchiesz <aschiesz91@gmail.com>
2017-09-07 08:27:03 -0400

    Add files via upload

---
+++ js/sidenav.js
@@ -0,0 +1,10 @@
+//Siteinfo to read details about site
+//function openInfo() {
+//	document.getElementById("bar").style.width = "250px";
+//	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+//}
+
+function closeInfo() {
+	document.getElementById("bar").style.width = "0";
+	document.body.style.backgroundColor = "white";
+}
